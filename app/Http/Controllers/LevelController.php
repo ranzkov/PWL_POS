709834e3d98ee1cab71aef607f5743420383b695
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-03-04 16:33:46 +0700

    Commit 4

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
+
+        // // Update data pada tabel m_level
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'cus']);
+
+        // // Pesan untuk menampilkan jumlah data yang diupdate
+        // $message = "Update data berhasil. Jumlah data yang diupdate: $row baris.";
+
+        // return $message;
+
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Delete data berhasil. Jumlah data yang dihapus:' . $row . ' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
+    }
+}
+
