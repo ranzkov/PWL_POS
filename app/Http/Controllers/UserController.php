87dda385d0ff53a68c3e08b55031b4ccb0d35674
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-03-10 15:06:51 +0700

    Commit Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,53 +10,53 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
-        // $user = UserModel::firstWhere('level_id', 1)->first();
-        // $user = UserModel::find(1);
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-
-        // // Tambahkan data pengguna baru ke dalam tabel m_user
-        // UserModel::create($data);
-
-        // // Ambil semua data pengguna dari tabel m_user
-        // $users = UserModel::all();
-
-        // // Kirim data pengguna ke view 'user'
-        // return view('user', ['data' => $users]);
-
-
-        // // Update data user dengan Eloquent Model
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-
-        // UserModel::where('username', 'customer-1')->update($data); // Update data user
-
-        // // Coba akses model UserModel
-        // $users = UserModel::all(); // Ambil semua data dari tabel m_user
-        // return view('user', ['data' => $users]);
-
-        // Tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-
-        // UserModel::insert($data); // Tambahkan data ke tabel m_user
-
-        // // Coba akses model UserModel
-        // $users = UserModel::all(); // Ambil semua data dari tabel m_user
-        // return view('user', ['data' => $users]);
     }
 }
+// $user = UserModel::findOr(20, ['username', 'nama'], function () {
+//     abort(404);
+// });
+// $user = UserModel::firstWhere('level_id', 1)->first();
+// $user = UserModel::find(1);
+// $data = [
+//     'level_id' => 2,
+//     'username' => 'manager_tiga',
+//     'nama' => 'Manager 3',
+//     'password' => Hash::make('12345')
+// ];
+
+// // Tambahkan data pengguna baru ke dalam tabel m_user
+// UserModel::create($data);
+
+// // Ambil semua data pengguna dari tabel m_user
+// $users = UserModel::all();
+
+// // Kirim data pengguna ke view 'user'
+// return view('user', ['data' => $users]);
+
+
+// // Update data user dengan Eloquent Model
+// $data = [
+//     'nama' => 'Pelanggan Pertama',
+// ];
+
+// UserModel::where('username', 'customer-1')->update($data); // Update data user
+
+// // Coba akses model UserModel
+// $users = UserModel::all(); // Ambil semua data dari tabel m_user
+// return view('user', ['data' => $users]);
+
+// Tambah data user dengan Eloquent Model
+// $data = [
+//     'username' => 'customer-1',
+//     'nama' => 'Pelanggan',
+//     'password' => Hash::make('12345'),
+//     'level_id' => 4
+// ];
+
+// UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+// // Coba akses model UserModel
+// $users = UserModel::all(); // Ambil semua data dari tabel m_user
+// return view('user', ['data' => $users]);
