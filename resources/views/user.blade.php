07b5381cbdfeebd57701e40c4dcc008b5e20a955
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-03-11 21:49:50 +0700

    commit Attribute Changes

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,24 +5,19 @@
 </head>
 <body>
     <h1>Data User</h1>
-    
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>Jumlah Pengguna</th> --}}
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        {{-- @foreach ($data as $d) --}}
         <tr>
-            {{-- <td> {{ $userCount }}</td> --}}
             <td>{{ $data->user_id }}</td>
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr>
-        {{-- @endforeach --}}
     </table>
 </body>
 </html>
