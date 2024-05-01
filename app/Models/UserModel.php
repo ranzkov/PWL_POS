32bc71791a5243050a0d15fc5ee5f1b825ecc656
Fitriani Novita Maharani <fitrianinovitamaharani@gmail.com>
2024-05-02 01:30:59 +0700

    Commit praktikum 1 JS10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,21 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
+// use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Model implements \Illuminate\Contracts\Auth\Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Authenticatable;
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
 
-    protected $table = 'm_user'; 
-    protected $primaryKey = 'user_id'; 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
+
+
+    // public function level(): BelongsTo{
+    //     return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    // }
+// }
